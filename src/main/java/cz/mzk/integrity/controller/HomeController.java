ed02df0a24f9375b557aae28af5e338385834f68
Ermak Aleksei <alexej.ermak96@gmail.com>
2019-06-24 13:29:12 +0200

    Simple form to get document uuid

--- src/main/java/cz/mzk/integrity/controller/HomeController.java
+++ src/main/java/cz/mzk/integrity/controller/HomeController.java
@@ -3,6 +3,10 @@ package cz.mzk.integrity.controller;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -13,5 +17,16 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/examine_pid")
+    public String examineForm() {
+        return "examine";
+    }
+
+    @PostMapping("/examine_pid")
+    public String examinePID(@RequestParam(name = "uuid") String uuid, HttpServletRequest request) {
+        logger.info("get uuid: " + uuid);
+        return "redirect:/examine_pid";
     }
 }
