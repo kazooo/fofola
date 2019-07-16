cd85e131f08bfe343fb1d72a2d7a8fdf5ad58cc5
Aleksei Ermak <alexej.ermak96@gmail.com>
2019-07-16 13:43:55 +0200

    Add Docker containerizing  - automated Docker image building by Gradle  - complex docker-compose for containerized Fofola  - possibility to get global variables from container environment  - fix error message in web

--- src/main/java/cz/mzk/integrity/controller/RestExceptionHandler.java
+++ src/main/java/cz/mzk/integrity/controller/RestExceptionHandler.java
@@ -15,7 +15,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { Exception.class, Exception.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
 
-        String bodyOfResponse = "There is an error! Check the console!";
+        String bodyOfResponse = ex.getMessage();
 
         return handleExceptionInternal(ex, bodyOfResponse,
                 new HttpHeaders(), HttpStatus.CONFLICT, request);
