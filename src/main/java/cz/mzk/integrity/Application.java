dbf9bffff42b08dea56ff8bad5e1a977d523c24c
Ermak Aleksei <alexej.ermak96@gmail.com>
2019-10-01 11:49:42 +0200

    Add startup time on home page

--- src/main/java/cz/mzk/integrity/Application.java
+++ src/main/java/cz/mzk/integrity/Application.java
@@ -1,13 +1,20 @@
 package cz.mzk.integrity;
 
+import cz.mzk.integrity.configuration.StartupApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
 
 
 @SpringBootApplication//(scanBasePackages={"cz.mzk.integrity"})
 @EnableAsync
 public class Application {
+
+    private static final Logger LOG
+            = Logger.getLogger(Application.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
