2de04dd5f13a4632000bc950a6cb8055e639d047
Ermak Aleksei <alexej.ermak96@gmail.com>
2020-06-08 15:09:56 +0200

    Add client for RELS-EXT update

--- src/main/java/cz/mzk/fofola/processes/utils/FedoraClient.java
+++ src/main/java/cz/mzk/fofola/processes/utils/FedoraClient.java
@@ -1,16 +1,23 @@
 package cz.mzk.fofola.processes.utils;
 
 import com.squareup.okhttp.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.ws.rs.core.MultivaluedMap;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -21,19 +28,27 @@ import java.util.concurrent.TimeUnit;
 public class FedoraClient {
 
     private final String fedoraHost;
+    private final String fedoraUser;
+    private final String fedoraPassword;
     private final OkHttpClient okHttpClient;
     private final DocumentBuilder xmlParser;
     private final Transformer xmlTransformer;
+    private final Client relsExtClient;
 
     public FedoraClient(String fedoraHost, String fedoraUser, String fedoraPswd)
             throws ParserConfigurationException, TransformerConfigurationException {
         this.fedoraHost = fedoraHost;
+        this.fedoraUser = fedoraUser;
+        this.fedoraPassword = fedoraPswd;
         okHttpClient = createAuthenticatedClient(fedoraUser, fedoraPswd);
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         xmlParser = factory.newDocumentBuilder();
         xmlTransformer = TransformerFactory.newInstance().newTransformer();
         xmlTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        relsExtClient = Client.create();
+        relsExtClient.setConnectTimeout(600000);
+        relsExtClient.setReadTimeout(600000);
     }
 
     private static OkHttpClient createAuthenticatedClient(final String username, final String password) {
@@ -78,8 +93,24 @@ public class FedoraClient {
     }
 
     public void setRelsExt(String uuid, Document relsExt) throws TransformerException, IOException {
-        setDatastream(uuid, "RELS-EXT", relsExt,
-                "X", "false", "A", "application/rdf+xml");
+        String url = fedoraHost + "/objects/" + uuid + "/datastreams/RELS-EXT";
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.add("controlGroup", "X");
+        queryParams.add("versionable", "false");
+        queryParams.add("dsState", "A");
+        queryParams.add("mimeType", "application/rdf+xml");
+        File tempDom = File.createTempFile("relsExt", ".rdf");
+        TransformerFactory.newInstance().newTransformer().transform(new DOMSource(relsExt), new StreamResult(tempDom));
+
+        WebResource resource = relsExtClient.resource(url);
+        BasicAuthenticationFilter credentials = new BasicAuthenticationFilter(fedoraUser, fedoraPassword);
+        resource.addFilter(credentials);
+        try {
+            resource.queryParams(queryParams).post(ClientResponse.class, tempDom);
+        } catch (UniformInterfaceException e) {
+            int status = e.getResponse().getStatus();
+            throw new IOException("Cannot set RELS-EXT data for " + uuid + ", status: " + status);
+        }
     }
 
     public void setDc(String uuid, Document dc) throws IOException, TransformerException {
@@ -116,6 +147,7 @@ public class FedoraClient {
     }
 
     public void close() {
+        relsExtClient.destroy();
         okHttpClient.getConnectionPool().evictAll();
     }
 }
