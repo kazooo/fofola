8e87081ed38636a0e56e4d5c7bbdfe26ee5d0134
Aleksei Ermak <alexej.ermak96@gmail.com>
2021-02-11 12:49:28 +0100

    Remove all whitespaces and non-visible characters from incoming uuids

--- src/main/java/cz/mzk/fofola/service/UuidService.java
+++ src/main/java/cz/mzk/fofola/service/UuidService.java
@@ -3,6 +3,7 @@ package cz.mzk.fofola.service;
 public class UuidService {
 
     public static String checkAndMakeUuid(String uuid) {
+        uuid = uuid.replaceAll("\\s+","");
         return uuid.startsWith("uuid:") ? uuid : "uuid:" + uuid;
     }
 }
