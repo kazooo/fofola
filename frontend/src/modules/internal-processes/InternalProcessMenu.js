60f500ff57f2af99e23fa0ab87b5408b442ce0e6
Aleksei Ermak <alexej.ermak96@gmail.com>
2021-11-27 01:25:38 +0100

    Add refresh button to process pages

--- frontend/src/modules/internal-processes/InternalProcessMenu.js
+++ frontend/src/modules/internal-processes/InternalProcessMenu.js
@@ -3,24 +3,37 @@ import {useEffect, useState} from "react";
 import {useInterval} from "../../effects/useInterval";
 import {Checkbox} from "../../components/form/Checkbox";
 import {requestInternalProcesses, requestNewPageInternalProcesses} from "./saga";
+import {Box, Grid} from "@material-ui/core";
+import {RefreshIconButton} from "../../components/button/iconbuttons";
 
 export const InternalProcessMenu = () => {
-
     const dispatch = useDispatch();
     const [autoReload, setAutoReload] = useState(true);
     const RELOAD_INTERVAL_MS = 5000;
 
     useEffect(() => {
         dispatch(requestNewPageInternalProcesses());
-    });
+    }, []);
 
     useInterval(() => {
         dispatch(requestInternalProcesses());
     }, autoReload ? RELOAD_INTERVAL_MS : null);
 
-    return <Checkbox
-        label={"Načítat automaticky (interval: 5 sec.)"}
-        onChange={() => setAutoReload(!autoReload)}
-        checked={autoReload}
-    />;
+    return (
+        <Grid container spacing={2} alignItems={"center"}>
+            <Grid item>
+                <Checkbox
+                    label={"Načítat automaticky (interval: 5 sec.)"}
+                    onChange={() => setAutoReload(!autoReload)}
+                    checked={autoReload}
+                />
+            </Grid>
+            <Grid item>
+                <RefreshIconButton
+                    onClick={() => dispatch(requestInternalProcesses())}
+                    tooltip={"Obnovit stránku procesů"}
+                />
+            </Grid>
+        </Grid>
+    );
 };
--- frontend/src/modules/kramerius-procesess/KrameriusProcessMenu.js
+++ frontend/src/modules/kramerius-procesess/KrameriusProcessMenu.js
@@ -1,26 +1,40 @@
-import {Checkbox} from "../../components/form/Checkbox";
-import {useDispatch} from "react-redux";
-import {useInterval} from "../../effects/useInterval";
-import {requestNewPageProcessesInfo, requestProcessesInfo} from "./saga";
+import {Grid} from "@material-ui/core";
 import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+
+import {requestNewPageProcessesInfo, requestProcessesInfo} from "./saga";
+import {RefreshIconButton} from "../../components/button/iconbuttons";
+import {Checkbox} from "../../components/form/Checkbox";
+import {useInterval} from "../../effects/useInterval";
 
 export const KrameriusProcessMenu = () => {
-
     const dispatch = useDispatch();
     const [autoReload, setAutoReload] = useState(true);
     const RELOAD_INTERVAL_MS = 5000;
 
     useEffect(() => {
         dispatch(requestNewPageProcessesInfo());
-    });
+    }, []);
 
     useInterval(() => {
         dispatch(requestProcessesInfo());
     }, autoReload ? RELOAD_INTERVAL_MS : null);
 
-    return <Checkbox
-        label={"Načítat automaticky (interval: 5 sec.)"}
-        onChange={() => setAutoReload(!autoReload)}
-        checked={autoReload}
-    />;
+    return (
+        <Grid container spacing={2} alignItems={"center"}>
+            <Grid item>
+                <Checkbox
+                    label={"Načítat automaticky (interval: 5 sec.)"}
+                    onChange={() => setAutoReload(!autoReload)}
+                    checked={autoReload}
+                />
+            </Grid>
+            <Grid item>
+                <RefreshIconButton
+                    onClick={() => dispatch(requestProcessesInfo())}
+                    tooltip={"Obnovit stránku procesů"}
+                />
+            </Grid>
+        </Grid>
+    );
 };
