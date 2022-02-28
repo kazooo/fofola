c977bc2e0171dca8b89981ec508de3ce5af76eed
Aleksei Ermak <aleksei.ermak@morosystems.cz>
2022-02-28 20:47:43 +0100

    Upgrade Java version, add Sugo session rows formatting

--- frontend/src/modules/dnnt-session/constants.js
+++ frontend/src/modules/dnnt-session/constants.js
@@ -22,38 +22,42 @@ export const columns = [
         label: 'Směr',
         width: 100,
         align: 'center',
+        format: value => Object.values(SugoSessionDirection).find(direction => direction.value === value).text,
     },
     {
         id: 'requestor',
         label: 'Requestor',
         width: 100,
         align: 'center',
+        format: value => Object.values(SugoSessionRequestor).find(requestor => requestor.value === value).text,
     },
     {
         id: 'operation',
         label: 'Operace',
         width: 100,
         align: 'center',
+        format: value => Object.values(SugoSessionOperation).find(operation => operation.value === value).text,
     },
     {
         id: 'status',
         label: 'Status',
         width: 100,
         align: 'center',
+        format: value => Object.values(SugoSessionStatuses).find(status => status.value === value).text,
     },
 ];
 
 export const SugoSessionDirection = Object.freeze({
     Rest2Dst: {
-        text: 'REST_2_DST',
+        text: 'Požadavek uživatele',
         value: 'REST_2_DST',
     },
     Src2Dst: {
-        text: 'SRC_2_DST',
+        text: 'Synchronizace SDNNT záznamů',
         value: 'SRC_2_DST',
     },
     Dst2Src: {
-        text: 'DST_2_SRC',
+        text: 'Synchronizace interních záznamů',
         value: 'DST_2_SRC',
     },
     Any: {
@@ -64,11 +68,11 @@ export const SugoSessionDirection = Object.freeze({
 
 export const SugoSessionRequestor = Object.freeze({
     Auto: {
-        text: 'AUTO',
+        text: 'Automatická labelizace',
         value: 'AUTO',
     },
     Rest: {
-        text: 'REST',
+        text: 'Uživatel',
         value: 'REST',
     },
     Any: {
@@ -78,24 +82,20 @@ export const SugoSessionRequestor = Object.freeze({
 });
 
 export const SugoSessionOperation = Object.freeze({
-    Search: {
-        text: 'SEARCH',
-        value: 'SEARCH',
-    },
     AddLabel: {
-        text: 'ADD_LABEL',
+        text: 'Přídání labelu',
         value: 'ADD_LABEL',
     },
     RemoveLabel: {
-        text: 'REMOVE_LABEL',
+        text: 'Odebirání labelu',
         value: 'REMOVE_LABEL',
     },
     Clean: {
-        text: 'CLEAN_LABELS',
+        text: 'Čištění labelů',
         value: 'CLEAN_LABELS',
     },
     Synchronize: {
-        text: 'SYNCHRONIZE',
+        text: 'Synchronizace',
         value: 'SYNCHRONIZE',
     },
     Any: {
